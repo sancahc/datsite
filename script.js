6586585c5a0d03103b1dd4e39555ccbb02997ef4
Fabricio C Zuardi <fabricio@fabricio.org>
2018-07-21 13:10:41 -0300

    implement local counter

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // Learn more about using JavaScript to interact with webpages:
 // https://developer.mozilla.org/en-US/docs/Web/javascript
 
+const LUCAS_COUNT = 7;
+let lucasIncrement = 0;
+
 const COLORS = [
   "rebeccapurple",
   "magenta",
@@ -19,10 +22,13 @@ const COLORS = [
   "gold"
 ];
 const button = document.getElementById("change-background-color");
+const lucasButton = document.getElementById("increment-lucas");
 
 button.addEventListener("click", changeBackgroundColor);
+lucasButton.addEventListener("click", incrementLucas);
 
 document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("lucas-counter").innerText = LUCAS_COUNT;
   document.getElementById("url").innerText = window.location;
   document.getElementById("library-url").href = `beaker://library/${
     window.location
@@ -43,4 +49,11 @@ function changeBackgroundColor() {
   const newColor = COLORS[idx];
 
   document.body.style.background = newColor;
+}
+
+function incrementLucas() {
+  // if ITS OVER 9000! we have a problem
+  lucasIncrement = Math.min(9000, lucasIncrement + 1);
+  document.getElementById("lucas-counter").innerText =
+    LUCAS_COUNT + lucasIncrement;
 }
